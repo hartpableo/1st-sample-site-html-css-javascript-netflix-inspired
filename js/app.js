132a2ed6e27ec911c1df1e7975666dd307e3f0a1
hartpableo <hart.hpwebdev@gmail.com>
2021-05-13 09:08:06 +0800

    My 1st sample site
    This is my 1st Frontend sample site that I created with pure HTML, CSS, and Vanilla JavaScript.

---
+++ js/app.js
@@ -0,0 +1,135 @@
+// toggle navbar button
+const toggleBtn = document.querySelector('.toggleBtn');
+const navLinks = document.querySelector('.navLinks');
+
+toggleBtn.addEventListener('click',function() {
+    navLinks.classList.toggle('active');
+});
+
+// auto set year on footer
+const year = document.querySelector('.date');
+year.innerHTML = new Date().getFullYear();
+
+// fixed navbar and back-to-top button
+const navbar = document.querySelector('.navbar');
+const backToTop = document.querySelector('.backToTop');
+
+window.addEventListener('scroll',function() {
+    const navHeight = navbar.getBoundingClientRect().height;
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > navHeight + 200) { // nav bar
+        navbar.classList.add('fixedNav');
+    } else {
+        navbar.classList.remove('fixedNav');
+    }
+
+    if (scrollHeight > 500) { // back to top
+        backToTop.classList.add('showBackToTop');
+    } else {
+        backToTop.classList.remove('showBackToTop');
+    }
+});
+
+// accurate navigation
+const scrollLinks = document.querySelectorAll('.scrollLink');
+
+scrollLinks.forEach(function(link) {
+    link.addEventListener('click',function(event) {
+        event.preventDefault(); // prevent default scroll behavior
+        const id = event.currentTarget.getAttribute('href').slice(1); // navigate to specific section id
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const navContainerHeight = navLinks.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixedNav');
+        let position = element.offsetTop - navHeight;
+
+        if (!navHeight) {
+            position = position - navHeight;
+        };
+        if (navHeight > 82) {
+            position = position + navContainerHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        navLinks.classList.remove('active'); // close nav links after clicking and navigating
+    });
+});
+
+// Awesome tabs for about-section
+const tabBtns = document.querySelectorAll('.tabBtn');
+const about = document.querySelector('.aboutContainer');
+const aboutText = document.querySelectorAll('.content');
+
+about.addEventListener('click',function(event) {
+    const id = event.target.dataset.id;
+    
+    if (id) {
+        tabBtns.forEach(function(btn) {
+            btn.classList.remove('active'); // remove active from other buttons
+            event.target.classList.add('active'); // add active class to selected or clicked button
+        });
+        aboutText.forEach(function(article) {
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id); // add the corresponding content or text of the buttons
+        element.classList.add('active');
+    }
+});
+
+// slide or carousel for works section
+const carouselImgs = document.querySelectorAll('.carouselSlide img');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+
+carouselImgs.forEach(function(slide,index) {
+    slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click',function() {
+    counter++;
+    carousel();
+});
+prevBtn.addEventListener('click',function() {
+    counter--;
+    carousel();
+});
+
+function carousel() {
+    // working with buttons
+    if (counter < carouselImgs.length - 1) {
+        nextBtn.style.display = 'block';
+    } else {
+        nextBtn.style.display = 'none';
+    };
+    if (counter > 0) {
+        prevBtn.style.display = 'block';
+    } else {
+        prevBtn.style.display = 'none';
+    }
+    
+    // working with slides' movement
+    carouselImgs.forEach(function(slide) {
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+};
+
+// FAQs accordion
+const questions = document.querySelectorAll('.questionItems');
+
+questions.forEach(function(clickedQuestion) {
+    const btn = clickedQuestion.querySelector('.answerBtns');
+    btn.addEventListener('click',function() {
+        clickedQuestion.classList.toggle('showAnswers');
+        console.log(clickedQuestion)
+        questions.forEach(function(qItem) {
+            if (qItem !== clickedQuestion) {
+                qItem.classList.remove('showAnswers');
+            }
+        });
+    });
+});
